42cb145358fcc754048216509593e6d7d9b7390a
Zoku <rizaller.amolo@gmail.com>
2017-03-11 20:40:13 +0800

    Finish Search Bar

--- hb_db(1).sql
+++ hb_db(1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 09, 2017 at 03:33 AM
+-- Generation Time: Mar 11, 2017 at 01:39 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.9
 
@@ -80,8 +80,8 @@ INSERT INTO `items` (`item_id`, `coll_id`, `itemName`, `itemType`, `itemDesc`, `
 (3, 2, 'Jojo''s Bizzare Adventure Part 2: Battle Tendency', 'Manga', 'Joseph Joestar', 'Mint', 1, 'Trading', 75.25, '2017-03-04 14:39:18', 'Active'),
 (4, 1, 'Blood On the Tracks', 'Album', '2nd Best Album', 'Mint', 1, 'Not Trading', 0, '2017-03-04 23:33:01', 'Active'),
 (5, 2, 'Berserk Vol.1', 'Manga', 'Kentaro Miura''s classic.', 'Mint', 2, 'Trading', 120.43, '2017-03-05 23:58:00', 'Active'),
-(6, 2, 'Jojo''s Bizzare Adventure Part 3: Stardust Crusaders', 'Manga', 'Jotaro Kujo', 'Good', 2, 'Trading', 43.52, '2017-03-06 19:52:47', 'Active'),
-(7, 1, 'Highway 61 Revisited', 'Album', '', 'Acceptable', 1, 'Trading', 600.65, '2017-03-06 22:14:50', 'Active');
+(6, 2, 'Jojo''s Bizzare Adventure Part 3: Stardust Crusaders', 'Manga', 'Jotaro Kujo', 'Good', 1, 'Trading', 43.52, '2017-03-06 19:52:47', 'Active'),
+(7, 1, 'Highway 61 Revisited', 'Album', '', 'Acceptable', 0, 'Trading', 600.65, '2017-03-06 22:14:50', 'Active');
 
 --
 -- Triggers `items`
@@ -108,8 +108,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('RBXS76DojQ9iFBBBxLAX5ylR0nnoi6yQ', 1489069213, '{"cookie":{"originalMaxAge":null,"expires":null,"httpOnly":true,"path":"/"},"loginUser":"riz","loginUserId":2}'),
-('y-8AZmC3dG1A27bh0VGFVcCz2ho8SVrN', 1489112994, '{"cookie":{"originalMaxAge":null,"expires":null,"httpOnly":true,"path":"/"},"loginUser":"bob","loginUserId":1}');
+('4oZPYNtk2ZyYDHUBz3aojKfNDIs54E9r', 1489290446, '{"cookie":{"originalMaxAge":null,"expires":null,"httpOnly":true,"path":"/"},"loginUser":"bob","loginUserId":1}');
 
 -- --------------------------------------------------------
 
@@ -128,9 +127,9 @@ CREATE TABLE `tradeItems` (
 --
 
 INSERT INTO `tradeItems` (`item_id`, `itemQuant`, `trade_id`) VALUES
-(1, 1, 2),
-(2, 1, 3),
-(3, 1, 3);
+(6, 1, 1),
+(6, 1, 2),
+(4, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -145,16 +144,24 @@ CREATE TABLE `trades` (
   `tradeQuantity` int(11) NOT NULL,
   `tradeStatus` enum('Offer','Declined','Completed','Currently Trading') NOT NULL,
   `priceOffer` float DEFAULT NULL,
-  `tradeDesc` text
+  `tradeDesc` text,
+  `dateSent` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `trades`
 --
 
-INSERT INTO `trades` (`trade_id`, `item_id`, `user_id`, `tradeQuantity`, `tradeStatus`, `priceOffer`, `tradeDesc`) VALUES
-(2, 3, 1, 1, 'Offer', 50.45, 'Giff me JOJO!!'),
-(3, 1, 2, 1, 'Offer', 250.43, 'Jojo for Dylan');
+INSERT INTO `trades` (`trade_id`, `item_id`, `user_id`, `tradeQuantity`, `tradeStatus`, `priceOffer`, `tradeDesc`, `dateSent`) VALUES
+(2, 7, 2, 1, 'Completed', 600.65, 'I want to add this to a new album collection.', '2017-03-10 00:00:00');
+
+--
+-- Triggers `trades`
+--
+DELIMITER $$
+CREATE TRIGGER `tradeDateSent` BEFORE INSERT ON `trades` FOR EACH ROW SET NEW.dateSent = NOW()
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -164,11 +171,16 @@ INSERT INTO `trades` (`trade_id`, `item_id`, `user_id`, `tradeQuantity`, `tradeS
 
 CREATE TABLE `tradingStatus` (
   `trade_id` int(11) NOT NULL,
-  `owner_id` int(11) NOT NULL,
-  `trader_id` int(11) NOT NULL,
-  `ownerStatus` enum('Not Received','Received') NOT NULL,
-  `traderStatus` enum('Not Received','Received') NOT NULL
+  `ownerStatus` enum('Not Received','Received','Canceled') NOT NULL,
+  `traderStatus` enum('Not Received','Received','Canceled') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tradingStatus`
+--
+
+INSERT INTO `tradingStatus` (`trade_id`, `ownerStatus`, `traderStatus`) VALUES
+(2, 'Received', 'Received');
 
 -- --------------------------------------------------------
 
